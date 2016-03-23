fda26c9b138fc99b9310ff18e114507e88b02c5c
Shaunak Sen <shaunak1105@gmail.com>
2016-03-23 15:22:10 +0530

    anonymous functions and more

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 /**
  * Created by shaunak on 23/3/16.
  */
-console.log('bacon');
+function addNumber(a, b)
+{
+    return a+b;
+}
+
+function stupid()
+{}
+
+console.log(addNumber(7,10.5));
+console.log(stupid());
+
+// Remember: any function without explicit return statement is going to return undefined
+
+// set a var to an anonymous function
+
+var printloves = function()
+{
+    console.log("mini loves shona");
+}
+printloves();
+
+//when u store a function in a var u can basically use that var in other functions
+
+setTimeout(printloves, 5000);
